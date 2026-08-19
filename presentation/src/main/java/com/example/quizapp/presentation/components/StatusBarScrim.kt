ff3ee541e02eb27c4423061e172bab7fd792f537
EngHemn <hemnfarhad14@gmail.com>
2026-08-19 05:03:33 +0300

    feat: add StatusBarScrim component for status bar background styling

--- presentation/src/main/java/com/example/quizapp/presentation/components/StatusBarScrim.kt
+++ presentation/src/main/java/com/example/quizapp/presentation/components/StatusBarScrim.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
+// comeonent status bar
 @Composable
 fun StatusBarScrim(
     modifier: Modifier = Modifier,
